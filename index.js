5496281e3c16085e9b7e1c0b8557d974f71296e7
Echo Sky <echosky.dev@gmail.com>
2019-08-29 14:33:33 +0700

    facebook

--- index.js
+++ index.js
@@ -7,42 +7,42 @@ app.get('/', (req, res) => {
 });
 
 
-// var passport = require('passport')
-//     , FacebookStrategy = require('passport-facebook').Strategy;
+var passport = require('passport')
+    , FacebookStrategy = require('passport-facebook').Strategy;
 
+ÎÎÎ
+// Passport session setup.
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
 
-// // Passport session setup.
-// passport.serializeUser(function (user, done) {
-//     done(null, user);
-// });
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
 
-// passport.deserializeUser(function (obj, done) {
-//     done(null, obj);
-// });
+passport.use(new FacebookStrategy({
+    clientID: '901760419844459',
+    clientSecret: '7e9dc26782f560375256038ca1e0de7f',
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+}, function (accessToken, refreshToken, profile, done) {
+    User.findOrCreate({
+        facebookId: profile.id
+    }, function (err, user) {
+        if (err) { return done(err); }
+        done(null, user);
+    });
+}
+));
 
-// passport.use(new FacebookStrategy({
-//     clientID: '901760419844459',
-//     clientSecret: '7e9dc26782f560375256038ca1e0de7f',
-//     callbackURL: "http://localhost:3000/auth/facebook/callback"
-// }, function (accessToken, refreshToken, profile, done) {
-//     User.findOrCreate({
-//         facebookId: profile.id
-//     }, function (err, user) {
-//         if (err) { return done(err); }
-//         done(null, user);
-//     });
-// }
-// ));
-
-// app.get('/auth/facebook', passport.authenticate('facebook'));
-// app.get('/auth/facebook/callback',
-//     passport.authenticate('facebook', {
-//         successRedirect: '/',
-//         failureRedirect: '/login'
-//     }));
-// app.get('/auth/facebook',
-//     passport.authenticate('facebook', { scope: ['read_stream', 'publish_actions'] })
-// );
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    }));
+app.get('/auth/facebook',
+    passport.authenticate('facebook', { scope: ['read_stream', 'publish_actions'] })
+);
 
 
 
