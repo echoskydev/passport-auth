8bb0c29bb4ec7a61b5e619c8111ae26bd2cb3f2e
Echo Sky <echosky.dev@gmail.com>
2019-08-29 15:42:43 +0700

    callback

--- configuration/config.js
+++ configuration/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "facebook_api_key": "901760419844459",
     "facebook_api_secret": "7e9dc26782f560375256038ca1e0de7f",
-    "callback_url": "https://passport-authen.herokuapp.com/auth/facebook/callback",
+    "callback_url": "https://passport-authen.herokuapp.com/return",
     "use_database": false,
     "host": "localhost",
     "username": "root",
