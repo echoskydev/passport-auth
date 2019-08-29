5ccfc3fef3c4ce82163a194ed36ed64b80753174
Echo Sky <echosky.dev@gmail.com>
2019-08-29 15:34:34 +0700

    update

--- index.js
+++ index.js
@@ -1,60 +1,46 @@
 const express = require('express')
     , passport = require('passport')
     , FacebookStrategy = require('passport-facebook').Strategy
-    , session = require('express-session')
-    , cookieParser = require('cookie-parser')
-    , bodyParser = require('body-parser')
     , config = require('./configuration/config')
     , app = express();
 
 const PORT = process.env.PORT || 5000
 
-passport.serializeUser(function (user, done) {
-    done(null, user);
-});
 
-passport.deserializeUser(function (obj, done) {
-    done(null, obj);
-});
 passport.use(new FacebookStrategy({
     clientID: config.facebook_api_key,
     clientSecret: config.facebook_api_secret,
     callbackURL: config.callback_url
-}, function (accessToken, refreshToken, profile, done) {
-    User.findOrCreate({
-        facebookId: profile.id
-    }, function (err, user) {
-        if (err) { return done(err); }
-        done(null, user);
-    });
-}
-));
+}, function (accessToken, refreshToken, profile, cb) {
+    // In this example, the user's Facebook profile is supplied as the user
+    // record.  In a production-quality application, the Facebook profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    return cb(null, profile);
+}));
 
-
-
-
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({ secret: 'keyboard cat', key: 'sid' }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(__dirname + '/public'));
-app.get('/', function (req, res) {
-    res.render('index', { user: req.user });
-    // res.send('Hi, Passport');
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
 });
 
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+});
+
+app.get('/', function (req, res) {
+    res.send('Hi, Passport');
+});
+app.get('/auth/facebook',
+    passport.authenticate('facebook'));
+
 app.get('/auth/facebook/callback',
-    passport.authenticate('facebook', {
-        successRedirect: '/',
-        failureRedirect: '/login'
-    }), function (req, res) {
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
         res.redirect('/');
     });
+
 app.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
