c7001d5b9c5ffb132a2729b33b0ccbd4f55ee8b4
Echo Sky <echosky.dev@gmail.com>
2019-08-30 14:10:39 +0700

    edit route

--- controllers/facebook.controller.js
+++ controllers/facebook.controller.js
@@ -5,17 +5,14 @@ const passport = require('passport');
 class FacebookController {
     authenticate() {
         return new Promise((resolve, reject) => {
-            console.log('hi');
-            passport.authenticate('facebook');
+            
+            
         });
     }
 
     callback() {
         return new Promise((resolve, reject) => {
-            passport.authenticate('facebook', { failureRedirect: '/login' }),
-                function (req, res) {
-                    res ? resolve(res) : reject(err);
-                }
+           
         });
     }
 
--- routes/route.js
+++ routes/route.js
@@ -1,24 +1,48 @@
 const express = require('express')
-const passport = require('passport');
-// const { blogpost } = require('../controllers')
+const passportFacebook = require('passport');
+const Strategy = require('passport-facebook').Strategy;
+
+const config = {
+    "cookieSecret": "seedsoft",
+    "facebook": {
+        "app_id": "901760419844459",
+        "app_secret": "7e9dc26782f560375256038ca1e0de7f",
+        "callback": "https://passport-authen.herokuapp.com/api/v1/callback/facebook"
+    },
+    "twitter": {
+        "consumer_key": "akeyishere",
+        "consumer_secret": "mysecretisbetterthanyoursecret",
+        "callback": "http://localhost:3000/auth/twitter/callback"
+    }
+}
+
+const FacebookStrategy = new Strategy({
+    clientID: config.facebook.app_id,
+    clientSecret: config.facebook.app_secret,
+    callbackURL: config.facebook.callback
+}, function (accessToken, refreshToken, profile, cb) {
+    return cb(null, profile);
+});
+
+passportFacebook.use(FacebookStrategy);
 
 const router = express.Router()
 
 //Facebook
-const FacebookController = require('../controllers/facebook.controller');
-const Facebook = new FacebookController();
+// const FacebookController = require('../controllers/facebook.controller');
+// const Facebook = new FacebookController();
 router.get('/v1/auth/facebook',
-    passport.authenticate('facebook'));
+    passportFacebook.authenticate('facebook'));
 router.get('/v1/callback/facebook',
-    passport.authenticate('facebook', { failureRedirect: '/v1/auth/facebook' }),
+    passportFacebook.authenticate('facebook', { failureRedirect: '/v1/auth/facebook' }),
     function (req, res) {
-        res.redirect('/');
+        console.log(res);
     }
 );
 router.get('/v1/profile/facebook',
     require('connect-ensure-login').ensureLoggedIn(),
     function (req, res) {
-        res.render('profile', { user: req.user });
+        console.log(user);
     });
 
 // router.get('/v1/auth/facebook', (req, res) => res.sendAsyncApi(Facebook.authenticate()));
