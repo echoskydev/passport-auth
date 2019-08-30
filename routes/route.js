4a4a6b08ce161416255ef7e4442d11d9b32ae187
Echo Sky <echosky.dev@gmail.com>
2019-08-30 13:56:50 +0700

    edit route

--- routes/route.js
+++ routes/route.js
@@ -9,7 +9,19 @@ const FacebookController = require('../controllers/facebook.controller');
 const Facebook = new FacebookController();
 router.get('/v1/auth/facebook',
     passport.authenticate('facebook'));
+router.get('/v1/callback/facebook',
+    passport.authenticate('facebook', { failureRedirect: '/v1/auth/facebook' }),
+    function (req, res) {
+        res.redirect('/');
+    }
+);
+router.get('/v1/profile/facebook',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function (req, res) {
+        res.render('profile', { user: req.user });
+    });
+
 // router.get('/v1/auth/facebook', (req, res) => res.sendAsyncApi(Facebook.authenticate()));
-router.get('/v1/callback/facebook', (req, res) => res.sendAsyncApi(Facebook.callback()));
+// router.get('/v1/callback/facebook', (req, res) => res.sendAsyncApi(Facebook.callback()));
 
 module.exports = router
