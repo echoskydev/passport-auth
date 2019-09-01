66323305e1375a9f4f4831662959ab337002bef3
Echo Sky <echosky.dev@gmail.com>
2019-09-01 12:46:02 +0700

    update

---
+++ routes/twitter.route.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport');
+
+const config = {
+    "cookieSecret": "seedsoft",
+    "facebook": {
+        "app_id": "901760419844459",
+        "app_secret": "7e9dc26782f560375256038ca1e0de7f",
+        "callback": "https://passport-authen.herokuapp.com/auth/facebook/callback"
+    },
+    "twitter": {
+        "consumer_key": "Gl76oTZWvoSyTWG5v7GY1xfcR",
+        "consumer_secret": "dVZdhTnj35MiyXAoGQkaofOYpotoMyOkLN6fURJAUDlnG90K9Z",
+        "callback": "https://passport-authen.herokuapp.com/auth/twitter/callback"
+    }
+}
+
+
+
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+router.use(passport.initialize());
+router.use(passport.session());
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+});
+
+
+
+
+
+const TwitterStrategy = require('passport-twitter').Strategy;
+passport.use(new TwitterStrategy({
+    consumerKey: config.twitter.consumer_key,
+    consumerSecret: config.twitter.consumer_secret,
+    callbackURL: config.twitter.callback
+}, function (accessToken, refreshToken, profile, cb) {
+    return cb(null, profile);
+}));
+
+
+
+// Define routes.
+router.get('/',
+    function (req, res) {
+        if (!req.user) {
+            res.render('home', { user: req.user });
+        } else {
+            res.json(req.user);
+        }
+        // res.render('home', { user: req.user });
+    });
+
+router.get('/login',
+    function (req, res) {
+        res.render('login');
+    });
+
+router.get('/auth/twitter',
+    passport.authenticate('twitter'));
+
+router.get('/auth/twitter/callback',
+    passport.authenticate('twitter', { failureRedirect: '/login' }),
+    function (req, res) {
+        res.redirect('/');
+    }
+);
+
+router.get('/profile',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function (req, res) {
+        res.json(req.user);
+        // res.render('profile', { user: req.user });
+    });
+
+
+router.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+
+module.exports = router
